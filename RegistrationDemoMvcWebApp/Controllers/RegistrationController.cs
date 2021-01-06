a0882697f244f44d40414f31f7916f6cee5ebcf0
tsinkunas <tsinkunas@gmail.com>
2021-01-06 09:50:06 +0200

    Add project files.

---
+++ RegistrationDemoMvcWebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistrationDemoMvcWebApp.Models;
+
+namespace RegistrationDemoMvcWebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Post(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Name = null;
+                model.Age = null;
+                model.Email = null;
+            }
+            return View("Index", model);
+        }
+    }
+}
---
+++ RegistrationDemoMvcWebApp/Models/RegistrationModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistrationDemoMvcWebApp.Models
+{
+    public class RegistrationModel
+    {
+        [Required(ErrorMessage = "Please enter your name!")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "The name must be between 2 and 30 characters long!")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$", ErrorMessage = "The name must be in Latin characters, and begin with a capital letter!")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter your age!")]
+        [Range(18, 100, ErrorMessage = "The age should be in 18 to 100 range!")]
+        public int? Age { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email!")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
